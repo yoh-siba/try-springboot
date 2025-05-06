88e7d5219183007ae642f33588fd63049212ad24
yoh-siba <otsuka20001220@gmail.com>
2025-05-06 16:12:04 +0900

    [FIX]不要なコードを削除

--- src/main/java/com/yohtsuka/hello/HelloApplicationController.java
+++ src/main/java/com/yohtsuka/hello/HelloApplicationController.java
@@ -1,10 +1,8 @@
 package com.yohtsuka.hello;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloApplicationController {
