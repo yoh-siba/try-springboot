980d44ea28c6205b97f48dcda3d035d410abeb00
yoh-siba <otsuka20001220@gmail.com>
2025-05-06 16:11:31 +0900

    [FIX]ReadMEを追加

---
+++ README.md
@@ -0,0 +1,5 @@
+[参考サイト](https://qiita.com/smats-rd/items/ec2dc566bfb4b92f04d5)
+
+curl http://localhost:8080
+
+curl -X POST http://localhost:8080/api/data -H "Content-Type: application/json" -d '{"name": "John"}
--- src/main/java/com/yohtsuka/hello/HelloApplicationController.java
+++ src/main/java/com/yohtsuka/hello/HelloApplicationController.java
@@ -1,13 +1,21 @@
 package com.yohtsuka.hello;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloApplicationController {
 
-    @RequestMapping("/")  // ルートへこのメソッドをマップする
+    @RequestMapping("/")
     public String test() {
         return "Hello World";
     }
+    // paramやメソッドを指定する場合（GET）
+    @RequestMapping(value="/test", method=RequestMethod.GET, headers="Accept=application/*", params="id=002")
+    public String testWithId() {
+        return "Test with ID 002";
+    } 
 }
